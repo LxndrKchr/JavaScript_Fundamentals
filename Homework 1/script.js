71a15d6d46d4bfc438fe721418dcdf7da2f87c6d
OlexandrKachur <lexandrkachur@gmail.com>
2022-11-22 01:05:40 +0200

    Homework 1

---
+++ Homework 1/script.js
@@ -0,0 +1,73 @@
+// TASK 2
+// console.log("Kachur");
+
+
+// TASK 3
+// let firstVariable = 1;
+// let secondVariable = 2;
+// console.log(firstVariable, secondVariable);
+
+// firstVariable = secondVariable;
+// console.log(firstVariable, secondVariable);
+
+
+// TASK 4 
+// let object = {
+//     string: "string",
+//     number: 0,
+//     boolean: true,
+//     null: null,
+//     undefined: undefined,
+// }
+// console.log(object);
+
+
+// TASK 5
+// let isAdult = confirm("Are you adult?");
+// console.log(isAdult);
+
+
+// TASK 6
+// let firstName;
+// let lastName;
+// let studyGroup;
+// let birthYear;
+
+// firstName = "Oleksand";
+// lastName = "Kachur";
+// studyGroup = "Lv-730";
+// birthYear = 1998;
+
+// let marriage = false;
+
+// console.log(typeof birthYear);
+// console.log(typeof marriage);
+// console.log(typeof firstName);
+// console.log(typeof lastName);
+// console.log(typeof studyGroup);
+
+// let a = null;
+// let b = undefined;
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+
+// TASK 7 
+// let login = prompt("Plese enter your login", "User");
+// let email = prompt("Plese enter your email", "example@mail.com");
+// let password = prompt("Plese enter your password", "password");
+
+// alert(" Dear " + login + ", your email is " + email + ", your password is " + password + ".");
+
+
+// TASK 8
+// let hour = prompt("Enter a hour", "1");
+// alert( " In " + hour + " hours, " + + hour * 60 * 60 + " seconds ");
+
+// let day = prompt("Enter a day", "1");
+// alert( " In " + day + " days, " + + day * 60 * 60 * 24 + " seconds ");
+
+// let months = prompt("Enter a month", "1");
+// alert( " In " + months + " months, " + + months * 60 * 60 * 24 * 30.42 + " seconds ");
+
