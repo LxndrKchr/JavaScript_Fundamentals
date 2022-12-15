7f73c91bc8ea24740a569301ccd57ddab11c9e1c
OlexandrKachur <lexandrkachur@gmail.com>
2022-12-15 14:57:50 +0200

    Homework 6

---
+++ Homework 6/script.js
@@ -0,0 +1,81 @@
+// TASK 1
+// let test = document.querySelector('#test');
+// let test = document.getElementById('test');
+// test.innerHTML = "Last";
+
+
+// TASK 2
+// const image = document.querySelector('.image');
+// image.src = 'cat.jpg';
+
+// alert(image.outerHTML);
+
+
+// TASK 3
+// const text = document.querySelectorAll('#text>p');
+// text.forEach(function(item, index) {
+//     console.log(`Selector text ${index} : ${item.innerHTML}`);
+// });
+
+
+// TASK 4
+// const list = document.querySelector('#list');
+
+// let first = list[0];
+// let second = list[1];
+// let third = list[2];
+// let fourth = list[3];
+// let fifth = list[4];
+
+// const first = list.firstElementChild;
+// const fifth = list.lastElementChild;
+// const second = list.firstElementChild.nextElementSibling;
+// const fourth = list.lastElementChild.previousElementSibling;
+// const third = list.firstElementChild.nextElementSibling.nextElementSibling;
+
+// alert(first.innerHTML);
+// alert(fifth.innerHTML);
+// alert(second.innerHTML);
+// alert(fourth.innerHTML);
+// alert(third.innerHTML);
+
+
+// TASK 5
+// const header = document.querySelector('h1').style.backgroundColor = 'lightgreen';
+
+// const myDiv = document.querySelector('#myDiv');
+// myDiv.childNodes[1].style.fontWeight = 'bold';
+// myDiv.childNodes[1].style.fontWeight = 'bold';
+// myDiv.childNodes[3].style.color = 'red';
+// myDiv.childNodes[5].style.textDecorationLine = 'underline';
+// myDiv.childNodes[7].style.fontStyle = 'italic';
+
+// const myList = document.querySelector('#myList');
+// myList.style.display = 'flex';
+// myList.style.listStyle = 'none';
+
+// const span = document.querySelector('span').style.visibility = 'hidden';
+
+
+// TASK 6
+// const firstInput = document.querySelector('#input1');
+// const secondInput = document.querySelector('#input2');
+
+// firstInput.value = prompt('', 'first');
+// secondInput.value = prompt('', 'second');
+
+// [firstInput.value , secondInput.value] = [secondInput.value , firstInput.value];
+
+
+// TASK 7
+// const main = document.createElement('main');
+// main.className = 'mainClass check item';
+// document.body.prepend(main);
+
+// const div = document.createElement('div');
+// div.id = 'myDiv'
+// main.prepend(div);
+
+// const para = document.createElement('p');
+// para.innerHTML = 'First paragraph';
+// div.prepend(para);
